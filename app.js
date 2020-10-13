a928edd66fefc30154b6a62c7f06b4427df6608c
Vladimir Dyakonov <dyakonov.vladimir2011@yandex.ru>
2020-10-13 21:51:29 +0300

    modules helmet & rate-limiter added

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 dotenv.config();
 
 const { users, articles } = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { limiter } = require('./middlewares/limiter');
 const { pageNotFound, centralizedErrorHandler } = require('./errors/error-handler');
 const { mdbConnect } = require('./explorer.config');
 
 const { PORT = 3000, NODE_ENV, MDB_CON } = process.env;
 const app = express();
 
+app.use(helmet()); // для установки заголовков, связанных с безопасностью
 app.use(cookieParser());
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
@@ -25,6 +28,7 @@ mongoose.connect(NODE_ENV ? MDB_CON : mdbConnect, {
   useFindAndModify: false,
 });
 
+app.use(limiter); // ограничение числа запросов с одного IP в единицу времени
 app.use(requestLogger); // логгер запросов
 
 app.use('/', articles);
